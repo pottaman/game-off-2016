5434f1063a563bf3ae72a240457016f843c7d4e1
Aleksandar Ružičić <aleksandar@ruzicic.info>
2016-11-02 04:37:52 +0100

    Setup MonoGame/Nez project

---
+++ Game.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nez;
+
+namespace Nirdala.GameOff2016 {
+
+    public class Game : Core {
+
+        public Game() : base(windowTitle: "Game Off 2016") {
+        }
+
+        protected override void Initialize() {
+            base.Initialize();
+
+            Window.AllowUserResizing = true;
+            Core.defaultSamplerState = SamplerState.LinearClamp;
+
+            Scene.setDefaultDesignResolution(1348, 900, Scene.SceneResolutionPolicy.NoBorder);
+
+            Core.scene = Scene.createWithDefaultRenderer(Color.Black);
+        }
+    }
+}
