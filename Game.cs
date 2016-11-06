ce150324eb541b2550877be1c17bb4325968d50e
Aleksandar Ružičić <aleksandar@ruzicic.info>
2016-11-06 06:08:07 +0100

    Implement BestFit scene resolution policy

--- Game.cs
+++ Game.cs
@@ -15,7 +15,7 @@ namespace Nirdala.GameOff2016 {
             Window.AllowUserResizing = true;
             Core.defaultSamplerState = SamplerState.LinearClamp;
 
-            Scene.setDefaultDesignResolution(1348, 900, Scene.SceneResolutionPolicy.NoBorder);
+            Scene.setDefaultDesignResolution(1348, 900, Scene.SceneResolutionPolicy.BestFit, 148, 140);
 
             Core.scene = Scene.createWithDefaultRenderer(Color.Black);
         }
