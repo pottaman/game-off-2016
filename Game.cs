5ec2fdf65ea82f60866e6aa7a3a87cfccf8c36b2
Aleksandar Ružičić <aleksandar@ruzicic.info>
2016-11-07 04:09:23 +0100

    Set initial window size based on monitor resolution

--- Game.cs
+++ Game.cs
@@ -6,16 +6,35 @@ namespace Nirdala.GameOff2016 {
 
     public class Game : Core {
 
-        public Game() : base(windowTitle: "Game Off 2016") {
-        }
+        const int WIDTH = 1348;
+        const int HEIGHT = 900;
+        const int BLEED_X = 148;
+        const int BLEED_Y = 140;
+        const float RATIO = (float)WIDTH / HEIGHT;
 
+        public Game() : base(windowTitle: "Game Off 2016") {
+
+            int width, height;
+
+            if (Screen.monitorWidth > Screen.monitorHeight) {
+                width = MathHelper.Min(WIDTH, (int)(Screen.monitorWidth * 0.75));
+                height = (int)(width / RATIO);
+            } else {
+                height = MathHelper.Min(HEIGHT, (int)(Screen.monitorHeight * 0.75));
+                width = (int)(height * RATIO);
+            }
+
+            Screen.preferredBackBufferWidth = width;
+            Screen.preferredBackBufferHeight = height;
+        }
+        
         protected override void Initialize() {
             base.Initialize();
 
             Window.AllowUserResizing = true;
             Core.defaultSamplerState = SamplerState.LinearClamp;
 
-            Scene.setDefaultDesignResolution(1348, 900, Scene.SceneResolutionPolicy.BestFit, 148, 140);
+            Scene.setDefaultDesignResolution(WIDTH, HEIGHT, Scene.SceneResolutionPolicy.BestFit, BLEED_X, BLEED_Y);
 
             Core.scene = Scene.createWithDefaultRenderer(Color.Black);
         }
